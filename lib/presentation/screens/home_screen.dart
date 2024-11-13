a6d64b6b63e2820fb967e95f80b393f3e7e15ab8
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-11-13 13:44:50 -0500

    add profile screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -22,7 +22,9 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: const CalendarWidget(),
-      bottomNavigationBar: const MenuWidget(),
+      bottomNavigationBar: const MenuWidget(
+        currentIndex: 1,
+      ),
       backgroundColor: Colors.grey[100],
     );
   }
