11b073d999bb7aad36b7d4737262a85aac3c14e0
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-11-19 18:06:13 -0500

    refactor: simplify LoginScreen layout and consolidate widgets

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -10,57 +10,51 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      title(),
-      const ImageWidget(image: "hermes.png"),
-      loginForm(),
-      ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue[700],
-          foregroundColor: Colors.white,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 30,
-            vertical: 10,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Iniciar sesión",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+            ),
           ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
+          const ImageWidget(image: "hermes.png"),
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FieldWidget(
+                hintText: "Correo",
+                oscureText: false,
+              ),
+              FieldWidget(
+                hintText: "Contraseña",
+                oscureText: true,
+              ),
+            ],
           ),
-        ),
-        onPressed: () {
-          // redirigir a la pantalla de home
-          Navigator.pushReplacementNamed(context, "/home");
-        },
-        child: const Text("Ingresar"),
-      )
-      // Add the login form here
-    ]));
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue[700],
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 10,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            onPressed: () {
+              // redirigir a la pantalla de home
+              Navigator.pushReplacementNamed(context, "/home");
+            },
+            child: const Text("Ingresar"),
+          )
+          // Add the login form here
+        ],
+      ),
+    );
   }
-}
-
-Widget title() {
-  return const Center(
-    child: Text(
-      "Iniciar sesión",
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 25,
-      ),
-    ),
-  );
-}
-
-Widget loginForm() {
-  return const Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      FieldWidget(
-        hintText: "Correo",
-        oscureText: false,
-      ),
-      FieldWidget(
-        hintText: "Contraseña",
-        oscureText: true,
-      ),
-    ],
-  );
 }
