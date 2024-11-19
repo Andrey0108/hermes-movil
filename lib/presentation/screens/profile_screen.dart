de04d36a10aaa0f9e041faf3300740d4d7e3f450
alan <alanandrey234@gmail.com>
2024-11-19 13:50:18 -0500

    remasterizacion apartado de perfil

--- lib/presentation/screens/profile_screen.dart
+++ lib/presentation/screens/profile_screen.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hermes/presentation/widgets/menu/menu_widget.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({
-    super.key,
-  });
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -20,52 +20,128 @@ class ProfileScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 10,
-        ),
-        child: const Column(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Nombre",
-              style: TextStyle(
-                fontSize: 20,
+            Container(
+              width: screenWidth * 0.7,
+              height: screenWidth * 0.7,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.grey[300],
+              ),
+              child: const Icon(
+                Icons.person,
+                size: 200,
+                color: Colors.white,
               ),
             ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Nombre",
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Email",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Email",
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Contraseña",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Contraseña",
-              ),
+            const SizedBox(height: 30),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Nombres: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const Text(
+                  "Esteban",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Apellidos: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const Text(
+                  "Quito",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Correo Electrónico: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const Text(
+                  "Estebanquito@gmail.com",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Teléfono: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const Text(
+                  "3111111111",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Dirección: ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const Text(
+                  "calle 99 99 99",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    // Implement logout logic here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 0, 88, 252),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Cerrar Sesión',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
