4d26c632fe5a6b513c78f4589a8a411459951920
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-11-20 21:57:36 -0500

    feat: implement PackageScreen with custom app bar and update calendar widget navigation

--- lib/presentation/screens/package_screen.dart
+++ lib/presentation/screens/package_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hermes/presentation/widgets/appbar_widget.dart';
 
 class PackageScreen extends StatelessWidget {
   const PackageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-      child: const Text('Package Screen'),
+    return Scaffold(
+      appBar: const AppBarWidget(
+        title: "Paquete",
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: const Text('Package Screen'),
+      ),
     );
   }
 }
