d48d77d0d7bfe71601c1271085e5827177c2a58e
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-06-01 13:10:09 -0500

    refactor logout function to remove unused code and improve clarity

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -31,9 +31,6 @@ Future<http.Response> login(String email, String password) async {
 }
 
 Future<bool> logout() async {
-  // final url = Uri.parse('$baseUrl/auth/log-out');
-  // final headers = {'Content-Type': 'application/json'};
-
   try {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('jwt_token');
@@ -42,12 +39,8 @@ Future<bool> logout() async {
       return false;
     }
 
-    // headers['Authorization'] = 'Bearer $token';
-
-    // final response = await http.post(url, headers: headers);
-    // if (response.statusCode == 200 || response.statusCode == 201) {
     await prefs.remove('jwt_token');
-    // }
+
     return true;
   } catch (e) {
     throw Exception('Error during logout: $e');
